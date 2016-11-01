d47c5a42de31def1819df6d889cdc338f6675a6c
Tony Song <tony99019@gmail.com>
2016-10-31 23:43:21 -0500

    Update Clojure to 1.9.0-alpha13

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 ;;   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
   :main ^:skip-aot clojure-experiment.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
